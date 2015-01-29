a0629c3750cdd82825a8e9d9139d3892cb6334e8
Arkadi Shishlov <arkadi.shishlov@gmail.com>
2015-01-29 23:58:54 +0200

    Use versioned images

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -xe
 
 godep go build -a github.com/GoogleCloudPlatform/heapster
+strip heapster
 
 docker build -t kubernetes/heapster .
-t=docker-registry.r53.acp.io:5000/kubernetes/heapster
+t=docker-registry.r53.acp.io:5000/kubernetes/heapster:2
 docker tag -f kubernetes/heapster $t
 docker push $t
--- influx-grafana/influxdb/build.sh
+++ influx-grafana/influxdb/build.sh
@@ -2,5 +2,5 @@
 
 docker build -t kubernetes/heapster_influxdb .
 t=docker-registry.r53.acp.io:5000/kubernetes/heapster_influxdb
-docker tag -f kubernetes/heapster_influxdb $t
+docker tag -f kubernetes/heapster_influxdb $t:1
 docker push $t
