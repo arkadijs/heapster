cba463cd29cd0fb43409f73e0a27bb63a727cfd0
Arkadi Shishlov <arkadi.shishlov@gmail.com>
2015-02-09 22:36:10 +0200

    Add development services to the list of infra containers

--- influx-grafana/grafana/build.sh
+++ influx-grafana/grafana/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -xe
 
 docker build -t kubernetes/heapster_grafana .
-t=docker-registry.r53.acp.io:5000/kubernetes/heapster_grafana:2
+t=docker-registry.r53.acp.io:5000/kubernetes/heapster_grafana:3
 docker tag -f kubernetes/heapster_grafana $t
 docker push $t
