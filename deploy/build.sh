682128f2f2ab191bf7807b963363fbfa9f07f776
Arkadi Shishlov <arkadi.shishlov@gmail.com>
2015-04-20 01:09:06 +0300

    Update Docker image

--- deploy/build.sh
+++ deploy/build.sh
@@ -4,6 +4,6 @@ godep go build -a github.com/GoogleCloudPlatform/heapster
 strip heapster
 
 docker build -t kubernetes/heapster .
-t=docker-registry.r53.acp.io:5000/kubernetes/heapster:4
+t=docker-registry.r53.acp.io:5000/kubernetes/heapster:5
 docker tag -f kubernetes/heapster $t
 docker push $t
